dee826dc9a1918b8bf53148a9fd9ea6fb37040ea
Tobias-Basilisco <tobiasbasilisco@gmail.com>
2025-11-14 12:41:41 +0100

    testNameWriting() set & tested

--- 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
+++ 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
@@ -16,6 +16,8 @@ class TestDeathNote {
 
     private static final int ACCEPTABLE_MESSAGE_LENGTH = 10;
     private final int NEGATIVE_VALUE = -5;
+    private static final String HUMAN_NAME = "Humanoid Johnson";
+    private static final String HUMAN_2_NAME = "Hum Anoid";
     private DeathNote deathNote;
     private String instruction = null;
 
@@ -27,7 +29,8 @@ class TestDeathNote {
         deathNote = new DeathNoteImpl();
     }
 
-    /*
+    //  1)
+    /* 
      * helper that tests IllegalArgumentException for values out of range of the instruction list
      */
     private void testInstructionsOutOfRange(final int value){
@@ -59,9 +62,10 @@ class TestDeathNote {
         testInstructionsOutOfRange(0);
     }
 
-     /*
-     * helper that tests legal values of the instruction list
-     */
+    //  2)
+    /*
+    * helper that tests legal values of the instruction list
+    */
     private void testInstructionsInRange(final int value){
         assertNotNull(deathNote.getRule(value));
         assertFalse(deathNote.getRule(value).isBlank());
@@ -76,4 +80,27 @@ class TestDeathNote {
             testInstructionsInRange(i);
         }
     }
+
+    //  3)
+    /**
+     * verifies that the human has not been written in the notebook yet
+     * writes the human in the notebook
+     * verifies that the human has been written in the notebook
+     * verifies that another human has not been written in the notebook
+     * verifies that the empty string has not been written in the notebook
+    */
+    @Test
+    public void testNameWriting(){
+        assertFalse(deathNote.isNameWritten(HUMAN_NAME));
+        try {
+            deathNote.writeName(HUMAN_NAME);
+        } catch (Exception e) {
+            Assertions.fail("No exception should be thrown for first name written: " + HUMAN_NAME);
+        }
+        assertTrue(deathNote.isNameWritten(HUMAN_NAME));
+        assertFalse(deathNote.isNameWritten(HUMAN_2_NAME));
+        assertFalse(deathNote.isNameWritten(""));
+    }
+
+
 }
