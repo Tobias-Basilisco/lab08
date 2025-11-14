94bbcd85026459675ee4f6fcb357b6a62c8cf813
Tobias-Basilisco <tobiasbasilisco@gmail.com>
2025-11-14 11:21:07 +0100

    Test imports

--- 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
+++ 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
@@ -1,5 +1,12 @@
 package it.unibo.deathnote;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 class TestDeathNote {
 
+    @BeforeEach
+    public void setUp(){
+        
+    }
 }
